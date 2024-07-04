ae33c628f6569051c9cfd94572070d02b3c7eee5
Dairymateo <seoxyt15@gmail.com>
2024-07-04 08:19:18 -0400

    cambios en repositoryo y viewMOdel

---
+++ Repositorios/CountryRepositorio.cs
@@ -0,0 +1,57 @@
+﻿using AVFoundation;
+using ExamPro3MateoHerrera.Modelos;
+using Newtonsoft.Json;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace ExamPro3MateoHerrera.Repositorios
+{
+    public class CountryRepositorio
+    {
+        public string _dbPath;
+        private SQLiteConnection conn;
+        private void Init()
+        {
+            if (conn != null)
+                return;
+
+
+            conn = new SQLiteConnection(_dbPath);
+            conn.CreateTable<CountryRepositorio>();
+
+
+
+
+
+        }
+
+        public void GuardarCountry
+
+
+
+        public CountryRepositorio (string dbPath) 
+        {
+            _dbPath = dbPath;
+        }
+
+        public async Task <Country> DevuelveCOuntryAsync() 
+        {
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync("https://restcountries.com/v3.1/all");
+            string response_json = await response.Result.Content.ReadAsStringAsync();
+
+            Country country = JsonConvert.DeserializeObject<Country>(response_json);
+
+            return country;
+        }
+
+
+        
+    }
+}
---
+++ ViewModels/CountryViewModel.cs
@@ -0,0 +1,37 @@
+﻿using ExamPro3MateoHerrera.Modelos;
+using ExamPro3MateoHerrera.Repositorios;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ExamPro3MateoHerrera.ViewModels
+{
+    public class CountryRepositorio
+    {
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+        
+
+
+
+}
