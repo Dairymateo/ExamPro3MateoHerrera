bceb6df55c8de4d60bb9316acc3164482b658108
Dairymateo <seoxyt15@gmail.com>
2024-07-04 07:34:02 -0400

    Creacion del Modelo

---
+++ Modelos/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPro3MateoHerrera.Modelos
+{
+    public class Country
+    {
+        public class Country
+        {
+            [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Region { get; set; }
+            public string Subregion { get; set; }
+            public string Status { get; set; }
+            public string Code { get; set; } 
+        }
+
+    }
+}
