0d5560b51134500643b6b70c6cb2b14cee742a21
Dairymateo <seoxyt15@gmail.com>
2024-07-04 07:33:00 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamPro3MateoHerrera
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
